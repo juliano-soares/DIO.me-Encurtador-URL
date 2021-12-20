1bade39cf0df4ce6f38a3ebcb91ec0b95fc391f5
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2021-12-19 23:11:17 -0300

    Finalizado o curso

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION: "mongodb+srv://<USER>:<PASSWORD>@cluster0.qwohx.mongodb.net/<DATABASE>?retryWrites=true&w=majority"
+}
